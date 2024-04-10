f6efd867c4e30e709858721ccc1cb53de8347fcd
Jonas Friberg <jonas.friberg@skola.taby.se>
2024-04-10 16:22:21 +0200

    lade till kod (behöver nog snabba till lite)

--- js.js
+++ js.js
@@ -1,7 +1,35 @@
 let canvas = document.getElementById("canvas")
-canvas.width = 800
+const context = canvas.getContext("2d")
+canvas.width = 700
 canvas.height = 400
 
 let movespeed = 10
-let sidespeed = 2
-let heightspeed = 2
+let xspeed = 5
+let yspeed = 2
+
+let xpos = 100
+let ypos = 100
+
+let circlesize = 20
+
+//while (canvas.width === 800) {
+
+   // window.requestAnimationFrame(rendercircle)
+   // window.requestAnimationFrame(renderspikes)
+
+//}
+
+window.requestAnimationFrame(rendercircle)
+function rendercircle(){
+
+    context.clearRect(0,0, screen.width, screen.height)
+
+    context.fillStyle = "red"
+
+    context.beginPath();
+    context.arc(xpos, ypos, circlesize, 0, 2 * Math.PI);
+    context.stroke();
+    
+   
+
+}
