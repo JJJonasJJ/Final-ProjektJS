6520d084c2ae5e2fbf66348dca6ceb2c826bc804
Jonas Friberg <jonas.friberg@skola.taby.se>
2024-04-17 10:50:02 +0200

    oops glömde lägga upp igår

--- js.js
+++ js.js
@@ -3,26 +3,73 @@ const context = canvas.getContext("2d")
 canvas.width = 700
 canvas.height = 400
 
-let movespeed = 10
-let xspeed = 5
-let yspeed = 2
+let movespeed = 1
+let xspeed = 2
+let yspeed = -0
 
 let xpos = 100
-let ypos = 100
+let ypos = 200
 
 let circlesize = 20
 
-//while (canvas.width === 800) {
 
-   // window.requestAnimationFrame(rendercircle)
-   // window.requestAnimationFrame(renderspikes)
+function canvasedgecheck(){
 
-//}
+    if (xpos < 21 || xpos > (canvas.width - circlesize)){
 
-window.requestAnimationFrame(rendercircle)
+        xspeed = -xspeed
+
+    }
+    if (ypos < 21 || ypos > (canvas.height - circlesize)){
+
+        yspeed = -yspeed
+
+    }
+
+}
+
+document.onkeydown = function (event){
+
+let key = e.key
+    switch (key) { 
+        case ("a" || "A"):
+            xspeed -= movespeed
+            break
+        case ("d" || "D"):
+            xspeed += movespeed
+            break
+        case " ":
+            if (yspeed < 0) {
+                yspeed += 2*movespeed
+                break
+            }
+            else{
+                yspeed -= 2*movespeed
+                break
+            }
+        default:
+            alert("Bara tangenterna 'a' 'd' 'mellanslag' kan användas.")
+}
+
+document.onkeyup = function (event) {
+    const key = event.key
+    switch (key){
+        case "a" || "A":
+            xspeed = 0
+            break
+        case "d" || "D":
+            xspeed = 0
+    }
+}
+} 
+
+
 function rendercircle(){
 
-    context.clearRect(0,0, screen.width, screen.height)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    xpos += xspeed
+    ypos += yspeed
 
     context.fillStyle = "red"
 
@@ -30,6 +77,8 @@ function rendercircle(){
     context.arc(xpos, ypos, circlesize, 0, 2 * Math.PI);
     context.stroke();
     
-   
 
+    canvasedgecheck()
+    window.requestAnimationFrame(rendercircle)
 }
+window.requestAnimationFrame(rendercircle)
